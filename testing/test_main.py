7bf8d10080e2883068cc09a3d4155e51f9dbaa3a
Gareth Tribello <gareth.tribello@gmail.com>
2022-09-09 17:59:27 +0100

    Fixed tests of code

--- testing/test_main.py
+++ testing/test_main.py
@@ -13,9 +13,15 @@ from main import *
 
 class UnitTests( unittest.TestCase ): 
    def test_rate_func(self):
-      xv, yv = np.linspace( 1, 1200, 1200 ), np.zeros(1200)
-      for i in range(1200) : 
-          vv = xv[i] - np.floor( xv / 180 )*180
-          if vv<60 : yv[i] = 0.5 
-          else : yv[i] =1/6 
-      assert( check_func( "rate", , ) )
+      xv, yv = [], [] 
+      for i in range(1200) :
+          xv.append((i,)) 
+          vv = i - np.floor( i / 600 )*600
+          if vv<60 : yv.append(0.5)
+          elif vv<180 : yv.append(1/6)
+          elif vv<240 : yv.append(0.5)
+          elif vv<360 : yv.append(1/6) 
+          elif vv<420 : yv.append(0.5)
+          elif vv<540 : yv.append(1/6)
+          else : yv.append(0.5)
+      assert( check_func( "rate", xv, yv ) )
